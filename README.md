21a8dc334b91f9905e1d692124bf4f47c46db56f
midorimaci <55018122+midorimaci@users.noreply.github.com>
2019-11-30 12:25:24 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-project
+
+https://compassionate-edison-b8784a.netlify.com/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-import sum from './js/sum';
-import './styles/style.scss'
-console.log(sum(1,2))
-console.log('Proof that it works')
+import sum from "./js/sum";
+import "./styles/style.scss";
+import cities from "./js/cities";
+console.log(sum(1, 2));
+console.log("Proof that it works");
+
+console.log(cities);
+
+const locationSelect = document.getElementById("location-select");
+
+locationSelect.addEventListener("change", event => {
+  const city = event.target.value;
+  const defaulCity = "img/skyline.jpg";
+  const cityImage = document.getElementById("location-img");
+  localStorage.setItem("cityImage", cityImage);
+  console.log(localStorage);
+
+  if (!cities[city]) {
+    cityImage.src = defaulCity;
+    return;
+  }
+
+  cityImage.src = cities[city].url;
+});
+
+fetch("6b6eaf2f6de7056f1c44e9569ca027ee")
+  .then(response => {
+    return response.json();
+  })
+  .then(result => {
+    console.log(result);
+  });
